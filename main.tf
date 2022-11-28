60af7314b5a3348f46efeca85c6e35682511339f
Mohamed Khaled <mohamedk1995@hotmail.co.uk>
2022-11-28 17:03:02 +0000

    add security group resource and key-pair

--- main.tf
+++ main.tf
@@ -4,11 +4,23 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "example" {
-  ami           =  "ami-05a82824508269098" 
-  instance_type = "t2.micro"
 
+
+resource "aws_instance" "example" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = var.key_name
   tags = {
-    Name = "terraform-example"
+    Name = var.name
+  }
+}
+resource "aws_security_group" "instance" {
+  name = var.name
+  ingress {
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = var.allow_ssh_from_cidrs
   }
 }
